63d4f693a0e6ebc0288394926d41752e73298ae5
张浩斌 <newsclan@gmail.com>
2021-11-30 11:58:52 +0800

    Update ssh-reverse-check.sh

--- ssh-reverse-check.sh
+++ ssh-reverse-check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if curl my.domain.com|grep "502 Bad Gateway" ;
+USER=user
+DOMAIN=my.domain.com
+
+if curl $DOMAIN |grep "502 Bad Gateway" ;
 then
-        pid=`ps aux|grep user@domain|grep ssh|awk '{print $2}'`
+        pid=`ps aux|grep "$USER@$DOAMIN" |grep ssh|awk '{print $2}'`
         kill -9 $pid;
 else
-        echo "my.domain.com works fine"
+        echo "$DOMAIN works fine, see you later ."
 fi
