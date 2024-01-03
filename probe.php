3fdce91463699d56d0403271e68e8ddea7553d57
张浩斌 <newsclan@gmail.com>
2024-01-03 08:41:42 +0800

    Update probe.php

--- probe.php
+++ probe.php
@@ -1,7 +1,10 @@
 <?php
 
-                    $total_mem=1880;
-                    $total_swap=2048;
+                    $memory=array();
+                    exec("free -m|awk '{print $2 }'|tail -2",$memory);
+                    $total_mem=$memory[0];
+                    $total_swap=$memory[1];
+
                     $delay=2;
                     $logfile="/dev/shm/".basename(__FILE__,"php")."log";
                     $history=@file_get_contents($logfile);
