d89d3c2d78ff46d955a5eea886d51f684abaa2be
张浩斌 <newsclan@gmail.com>
2024-01-09 22:26:35 +0800

    Update bookpi.sh

--- bookpi.sh
+++ bookpi.sh
@@ -7,8 +7,11 @@ iface wlan0 inet dhcp
 EOF
 
 apt-get update
-apt-get install -y xinit lxde xserver-xorg pavucontrol pulseaudio alsa-utils
+apt-get install -y xinit lxde xserver-xorg pavucontrol pulseaudio alsa-utils mesa-utils p7zip-full xfce4
 apt-get install -y openssh-server 
 apt-get install -y quake3 dosbox prboom-plus doom-wad-shareware
 wget http://dl.hustoj.com/debian/quake3.deb
 dpkg -i quake3.deb
+
+glxinfo -B |grep Accelerated
+7z b
